06ed778c941a3c58db9d75678048c215b8a3fea5
Rohit Wadne <rohit.wadne@apisero.com>
2021-08-20 13:38:32 +0530

    1st

---
+++ src/main/java/com/mule/training/App.java
@@ -0,0 +1,14 @@
+package com.mule.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println( "Hello World Part 2" );
+    }
+}
